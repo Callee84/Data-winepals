8bb3f33d753579d264c4c02e4a58bff48717645c
Callee84 <carl.g.holm@gmail.com>
2022-12-26 08:27:50 +0000

    Add model for post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post (models.Model):
+    # model for creating a post.
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_wine_b693ou',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.id} | {self.title}'
